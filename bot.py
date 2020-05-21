4a32c4a30b196e697fa4527a6de8793b721a5fc7
vivi <me@vivi.fyi>
2020-05-21 17:44:24 +0100

    Made a bot

---
+++ bot.py
@@ -0,0 +1,55 @@
+import sys
+import traceback
+from os import environ
+
+import discord
+from discord.ext import commands
+
+extensions = ("ext.markdown",)
+
+
+def _prefix(bot, msg):
+    prefixes = (f"<@!{bot.user.id}> ", f"<@{bot.user.id}> ")
+    prefixes = (*prefixes, "d.")
+
+    return prefixes
+
+
+class Bot(commands.AutoShardedBot):
+    def __init__(self):
+        super().__init__(
+            command_prefix=_prefix,
+            description="Helper bot for Discohook <https://discohook.org/>",
+            activity=discord.Game(name="at discohook.org | d.help"),
+        )
+
+        for extension in extensions:
+            self.load_extension(extension)
+
+    async def on_ready(self):
+        print(f"Ready as {self.user} ({self.user.id})")
+
+    async def on_command_error(self, ctx, error):
+        if isinstance(error, commands.UserInputError):
+            await ctx.send(error)
+
+        elif isinstance(error, commands.CommandNotFound):
+            pass
+
+        else:
+            err = error
+            if isinstance(error, commands.CommandInvokeError):
+                err = error.original
+
+            if not isinstance(err, discord.HTTPException):
+                traceback.print_tb(err.__traceback__)
+                print(f"{err.__class__.__name__}: {err}", file=sys.stderr)
+
+
+def main():
+    bot = Bot()
+    bot.run(environ.get("DISCORD_TOKEN"))
+
+
+if __name__ == "__main__":
+    main()
---
+++ ext/markdown.py
@@ -0,0 +1,41 @@
+from discord.ext import commands
+
+
+class Markdown(commands.Cog):
+    """Markdown syntax helpers"""
+
+    @commands.command()
+    async def user(
+        self, ctx: commands.Context, member: commands.MemberConverter,
+    ):
+        """Gives formatting to mention a given member"""
+
+        await ctx.send(f"`{member.mention}`")
+
+    @commands.command()
+    async def role(
+        self, ctx: commands.Context, role: commands.RoleConverter,
+    ):
+        """Gives formatting to mention a given role"""
+
+        await ctx.send(f"`{role.mention}`")
+
+    @commands.command()
+    async def channel(
+        self, ctx: commands.Context, channel: commands.TextChannelConverter,
+    ):
+        """Gives formatting to link to a given channel"""
+
+        await ctx.send(f"`{channel.mention}`")
+
+    @commands.command(aliases=["emote"])
+    async def emoji(
+        self, ctx: commands.Context, emoji: commands.EmojiConverter,
+    ):
+        """Gives formatting to use a given server emoji"""
+
+        await ctx.send(f"`{emoji}`")
+
+
+def setup(bot):
+    bot.add_cog(Markdown(bot))
