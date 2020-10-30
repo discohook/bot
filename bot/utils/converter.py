084a00415737fcd0be20f56bc4c649f1f368e466
vivi <me@vivi.fyi>
2020-10-30 10:16:32 +0100

    Subclass discord.py's message converter

--- bot/utils/converter.py
+++ bot/utils/converter.py
@@ -7,58 +7,25 @@ from discord.utils import find, get
 from . import wrap_in_code
 
 
-class MessageConverter(commands.Converter):
+class MessageConverter(commands.MessageConverter):
     """Converts to a :class:`discord.Message`.
 
     Different from `discord.ext.commands.converter.MessageConverter` by not
     leaking messages from other guilds, or channels the member cannot read.
     """
 
-    def readable_by_member(self, ctx: commands.Context, message: discord.Message):
-        return (
-            message.guild == ctx.guild
-            and message.channel.permissions_for(ctx.author).read_messages
-            and message.channel.permissions_for(ctx.author).read_message_history
-        )
-
     async def convert(self, ctx: commands.Context, argument):
-        id_regex = re.compile(
-            r"^(?:(?P<channel_id>[0-9]{15,21})-)?(?P<message_id>[0-9]{15,21})$"
-        )
-        link_regex = re.compile(
-            r"^https?://(?:(ptb|canary)\.)?discord(?:app)?\.com/channels/"
-            r"(?:([0-9]{15,21})|(@me))"
-            r"/(?P<channel_id>[0-9]{15,21})/(?P<message_id>[0-9]{15,21})/?$"
-        )
+        message = await super().convert(ctx, argument)
 
-        match = id_regex.match(argument) or link_regex.match(argument)
-        if not match:
-            raise commands.MessageNotFound(argument)
+        perms = message.channel.permissions_for(ctx.author)
+        if (
+            message.guild == ctx.guild
+            and perms.read_messages
+            and perms.read_message_history
+        ):
+            return message
 
-        message_id = int(match.group("message_id"))
-        channel_id = int(match.group("channel_id") or 0)
-
-        message = ctx.bot._connection._get_message(message_id)
-        if message:
-            if self.readable_by_member(ctx, message):
-                return message
-
-            raise commands.ChannelNotReadable(message.channel)
-
-        channel = ctx.bot.get_channel(channel_id) if channel_id else ctx.channel
-        if not channel:
-            raise commands.ChannelNotFound(str(channel_id))
-
-        try:
-            message = await channel.fetch_message(message_id)
-            if self.readable_by_member(ctx, message):
-                return message
-
-            raise commands.ChannelNotReadable(channel)
-        except discord.NotFound:
-            raise commands.MessageNotFound(argument)
-        except discord.Forbidden:
-            raise commands.ChannelNotReadable(channel)
+        raise commands.ChannelNotReadable(message.channel)
 
 
 class WebhookConverter(commands.IDConverter):
