b7e30999ff5341cece3383b23d514b283f3c6eee
vivi <git@vivi.zone>
2022-09-03 20:32:59 +0200

    Force fetch if guild.me has no permissions

--- src/lib/guilds/getSelf.ts
+++ src/lib/guilds/getSelf.ts
@@ -4,5 +4,11 @@ export const getSelf = async (guild: Guild) => {
   if (!guild.me || guild.me.partial) {
     return guild.members.fetch(guild.client.user?.id!)
   }
+  if (guild.me.permissions.bitfield === 0n) {
+    // This smells, but zero permissions is very unusual and a good sign the
+    // cache is messed up.
+    await guild.fetch()
+    return guild.members.fetch({ user: guild.client.user?.id!, force: true })
+  }
   return guild.me
 }
