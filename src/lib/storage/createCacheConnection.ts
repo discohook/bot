f83b2ab8d0739dbf2a1d4f133560c4e747e5cece
vivi <git@vivi.zone>
2022-10-03 02:52:55 +0200

    Reconnect cache on timed out error

--- src/lib/storage/createCacheConnection.ts
+++ src/lib/storage/createCacheConnection.ts
@@ -1,3 +1,7 @@
 import Redis from "ioredis"
 
-export const createCacheConnection = () => new Redis(process.env.CACHE_URL!)
+export const createCacheConnection = () =>
+  new Redis(process.env.CACHE_URL!, {
+    keepAlive: 60000 as any,
+    reconnectOnError: (error) => error.message.includes("ETIMEDOUT"),
+  })
